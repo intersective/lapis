3dac7e32e1ed05c36c8b07608d1ac47fd4c1d87b
U-Zyn Chua <chua@uzyn.com>
2017-01-12 22:01:33 +0800

    Obtain ancestor keys.

--- Model/Key.php
+++ Model/Key.php
@@ -7,4 +7,36 @@ App::uses('AppModel', 'Model');
 class Key extends AppModel {
 	public $tablePrefix = 'lapis_';
 	public $name = 'Key';
+
+	/**
+	 * Return a list of ancestor IDs including self
+	 */
+	public function getAncestorIDs($ids, $includeSelf = true) {
+		$indexedRes = array();
+		$parentIDs = $ids;
+
+		while (!empty($parentIDs)) {
+			$keys = $this->find('list', array(
+				'conditions' => array('Key.id' => $parentIDs),
+				'fields' => array('Key.id', 'Key.parent_id')
+			));
+
+			$parentIDs = array();
+			if (!empty($keys)) {
+				foreach ($keys as $selfID => $parentID) {
+					if ($includeSelf && !isset($indexedRes[$selfID])) {
+						$indexedRes[$selfID] = true;
+					}
+					if (!empty($parentID)) {
+						array_push($parentIDs, $parentID);
+						if (!isset($indexedRes[$parentID])) {
+							$indexedRes[$parentID] = true;
+						}
+					}
+				}
+			}
+		}
+
+		return array_keys($indexedRes);;
+	}
 }
