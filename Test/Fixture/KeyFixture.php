edd0af8af66e8cbbfbfbd7cb29bcfb06892061f3
U-Zyn Chua <chua@uzyn.com>
2017-01-31 16:37:09 +0800

    Updates the test to only require 1 database instead of 2 (not polluting app's db)

--- Test/Fixture/KeyFixture.php
+++ Test/Fixture/KeyFixture.php
@@ -1,4 +1,15 @@
 <?php
 class KeyFixture extends CakeTestFixture {
-	public $import = 'Lapis.Key';
+	public $table = 'lapis_keys';
+
+	/**
+	 * Load from schema.php (DRY)
+	 */
+	public function init() {
+		$schemaPath = dirname(dirname(dirname(__FILE__))) . DS . 'Config' . DS . 'Schema' . DS . 'schema.php';
+		$LapisSchema = require($schemaPath);
+		$schema = new LapisSchema;
+		$this->fields = $schema->tables[$this->table];
+		parent::init();
+	}
 }
