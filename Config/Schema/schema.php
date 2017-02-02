c7143d4c6005c7980330b7c6294b91656dbf368f
U-Zyn Chua <chua@uzyn.com>
2017-02-02 18:28:49 +0800

    Schema update. #10

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -8,32 +8,44 @@ class LapisSchema extends CakeSchema {
 	public function after($event = array()) {
 	}
 
-	public $lapis_keys = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'key' => 'primary'),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
-		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+	public $lapis_requesters = array(
+		'id' => array('type' => 'binary', 'null' => false, 'default' => null, 'length' => 36, 'key' => 'primary'),
+		'created' => array('type' => 'datetime'),
+		'modified' => array('type' => 'datetime'),
 		'parent_id' => array('type' => 'integer', 'null' => true, 'default' => null),
-		'parent_id' => array('type' => 'integer', 'null' => true, 'default' => null),
-		'public_key' => array('type' => 'text', 'null' => false),
-		'private_key' => array('type' => 'text', 'null' => true, 'default' => null),
-		'active' => array('type' => 'boolean', 'null' => false, 'default' => true),
+		'ident_public_key' => array('type' => 'text', 'null' => false),
+		'ident_private_key' => array('type' => 'text', 'null' => true, 'default' => null),
+		'vault_public_key' => array('type' => 'text', 'null' => true, 'default' => null),
 		'indexes' => array(
 			'PRIMARY' => array('unique' => true, 'column' => 'id')
 		),
 		'tableParameters' => array()
 	);
 
-	public $lapis_documents = array(
-		'id' => array('type' => 'binary', 'null' => false, 'default' => null, 'length' => 256, 'key' => 'primary'),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
-		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
-		'key_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 11),
-		'model_id' => array('type' => 'binary', 'null' => false, 'default' => null, 'length' => 256),
-		'document_pw' => array('type' => 'text', 'null' => false),
+	public $lapis_vaults = array(
+		'id' => array('type' => 'binary', 'null' => false, 'default' => null, 'length' => 36, 'key' => 'primary'),
+		'created' => array('type' => 'datetime'),
+		'modified' => array('type' => 'datetime'),
+		'owner_requester_id' => array('type' => 'binary', 'null' => false, 'default' => null, 'length' => 36),
+		'accessor_requester_id' => array('type' => 'binary', 'null' => false, 'default' => null, 'length' => 36),
+		'vault_private_key' => array('type' => 'text', 'null' => true, 'default' => null),
 		'indexes' => array(
 			'PRIMARY' => array('unique' => true, 'column' => 'id'),
-			'lapis_key_id' => array('column' => 'key_id'),
-			'lapis_model_id' => array('column' => 'model_id'),
+			'lapis_vaults_owner_requester_id' => array('column' => 'owner_requester_id'),
+			'lapis_vaults_accessor_requester_id' => array('column' => 'accessor_requester_id')
+		),
+		'tableParameters' => array()
+	);
+
+	public $lapis_objects = array(
+		'id' => array('type' => 'binary', 'null' => false, 'default' => null, 'length' => 256, 'key' => 'primary'),
+		'created' => array('type' => 'datetime'),
+		'modified' => array('type' => 'datetime'),
+		'owner_requester_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 36),
+		'key' => array('type' => 'text', 'null' => false),
+		'indexes' => array(
+			'PRIMARY' => array('unique' => true, 'column' => 'id'),
+			'lapis_objects_owner_requester_id' => array('column' => 'owner_requester_id')
 		),
 		'tableParameters' => array()
 	);
